8040e633b2af338c57998c53d48cff944e47ac07
zexi yu <eezxyu@gmail.com>
2019-05-05 16:51:00 -0500

    Add a new component Input.js to handle email and password input. The label and onChangeText is given by its parent component. Reusable component  mainily focus on styles.

--- src/component/LoginForm.js
+++ src/component/LoginForm.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        };
+    }
     render() {
         return(
             <Card>
-                <CardSection />
-                <CardSection />
-                <CardSection >
-                    <Button>
-                        <Text>Log in</Text>
-                    </Button>
+                <CardSection>
+                    <Input placeholder='enter your email'
+                           value={this.state.email}
+                           onChangeText={email => this.setState({email})}
+                           label='Email'
+                    />
+                </CardSection>
+                <CardSection>
+                    <Input placeholder='enter your password'
+                           value={this.state.password}
+                           onChangeText={password => this.setState({password})}
+                           label='Password'
+                           secureTextEntry={true}
+                    />
+                </CardSection>
+                <CardSection>
+                    <Button>Log in</Button>
                 </CardSection>
             </Card>
         );
